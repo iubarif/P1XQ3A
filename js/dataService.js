957be4ae5636e817a45821ab71b322a9899a3751
iubarif <iubarif@yahoo.com>
2017-08-16 12:37:13 -0700

    Date bug fixed

--- js/dataService.js
+++ js/dataService.js
@@ -13,7 +13,7 @@ var testData = {
 }
 
 function getEvents(searchObject) {
-        
+
     $.ajax({
         url: _config.configs.eventServEndPoint,
         data: searchObject,
@@ -21,156 +21,70 @@ function getEvents(searchObject) {
         dataType: 'json',
         success: function (data) {
 
-            debugger;
-            
-            var markup = "<tr><th><div class='panel panel-primary'>Total Items found : {{total}}</div></th></tr>";
-            $("table tbody").append(markup.replace("{{total}}",data.total_items));
+            var eventRoot;
 
-            var ev = data.events.event;
+            if (data != null) {
+                eventRoot = $.extend(new _entity.EventfulRoot(), data);
 
-            $.each(ev, function (i, item) {
+                var markup = "<tr><th><div class='panel panel-primary'>Total Items found : {{total}}</div></th></tr>";
+                $("table tbody").append(markup.replace("{{total}}", eventRoot.total_items));
 
-                var event = $.extend(new _entity.Event(), ev[i]);
-                var template = _config.rowTemplate;
-                var day;
-                var month;
-                var year;
+                var ev = eventRoot.events.event;
 
-                var imgUrl = _config.configs.notFoundPicture;
+                $.each(ev, function (i, item) {
 
-                if (!$.isEmptyObject(event.image)) {
-                    if (!$.isEmptyObject(event.image.thumb)) {
-                        if (!$.isEmptyObject(event.image.thumb.url)) {
-                            imgUrl = event.image.thumb.url;
+                    var event = $.extend(new _entity.Event(), ev[i]);
+                    var template = _config.rowTemplate;
+                    var day;
+                    var month;
+                    var year;
+
+                    var imgUrl = _config.configs.notFoundPicture;
+
+                    if (!$.isEmptyObject(event.image)) {
+                        if (!$.isEmptyObject(event.image.thumb)) {
+                            if (!$.isEmptyObject(event.image.thumb.url)) {
+                                imgUrl = event.image.thumb.url;
+                            }
                         }
                     }
-                }
 
-                if (!$.isEmptyObject(event.start_time)) {
-                    var stratDate = new Date(event.start_time);
+                    if (!$.isEmptyObject(event.start_time)) {
+                        var stratDate = new Date(event.start_time);
 
-                    day = stratDate.getDate();
-                    month = _config.monthNames[stratDate.getMonth()];
-                    year = stratDate.getFullYear();
+                        day = stratDate.getDate();
+                        month = _config.monthNames[stratDate.getMonth()];
+                        year = stratDate.getFullYear();
 
-                    if (month < 10) month = "0" + month;
-                    if (day < 10) day = "0" + day;
-                }
+                        if (month < 10) month = "0" + month;
+                        if (day < 10) day = "0" + day;
+                    }
 
-                if (!$.isEmptyObject(event.description)) {
-                    event.description = event.description.length > 200 ? event.description.substring(0, 200) + "..." : event.description;
-                }
-                else{
-                    event.description = "";
-                }
-                event.image = imgUrl;
+                    if (!$.isEmptyObject(event.description)) {
+                        event.description = event.description.length > 200 ? event.description.substring(0, 200) + "..." : event.description;
+                    }
+                    else {
+                        event.description = "";
+                    }
+                    event.image = imgUrl;
 
-                template = template.replace('{{image}}', event.image);
-                template = template.replace('{{title}}', event.title);
-                template = template.replace('{{url}}', event.url);
-                template = template.replace('{{venue_name}}', event.venue_name);
-                template = template.replace('{{venue_url}}', event.venue_url);
-                template = template.replace('{{description}}', event.description);
-                template = template.replace('{{day}}', day);
-                template = template.replace('{{month}}', month);
-                template = template.replace('{{year}}', year);
+                    template = template.replace('{{image}}', event.image);
+                    template = template.replace('{{title}}', event.title);
+                    template = template.replace('{{url}}', event.url);
+                    template = template.replace('{{venue_name}}', event.venue_name);
+                    template = template.replace('{{venue_url}}', event.venue_url);
+                    template = template.replace('{{description}}', event.description);
+                    template = template.replace('{{day}}', day);
+                    template = template.replace('{{month}}', month);
+                    template = template.replace('{{year}}', year);
 
-                var markup = '<tr><td>{{template}}</td></tr>';
-                $("table tbody").append(markup.replace('{{template}}', template));
-            });
-        }                
-    });    
+                    var markup = '<tr><td>{{template}}</td></tr>';
+                    $("table tbody").append(markup.replace('{{template}}', template));
+                });
+            }
+        }
+    });
 }
-
-
-// function validateAddress(address) {
-//     //_util.showBusy(true);
-
-//     $.ajax({
-//         url: _config.configs.geocodeServiceEndPoint + address,
-//         type: 'get',
-//         dataType: 'json',
-//         success: function (data) {
-//             var langLat = "Lon : " + data.lng + " , Lat : " + data.lat;
-
-//             alert(langLat);
-
-//             return true;
-//         },
-//         error: function () {
-//             return false;
-//         }
-
-//     });
-//     // .done(function() {
-//     //     _util.showBusy(false);
-//     // });
-
-//     // var isSuccess = false;
-
-//     // $.ajax({
-//     //     url: _config.configs.geocodeServiceEndPoint + value,
-//     //     type: 'get',
-//     //     dataType: 'json',
-//     //     async: false,
-//     //     success: function (data) {
-//     //         //var langLat = "Lon : " + data.lng + " , Lat : " + data.lat;
-
-//     //         //alert(langLat);
-//     //         if (data) {
-//     //             isSuccess = true;
-//     //         }
-//     //     },
-
-//     // });
-
-//     // return isSuccess;
-// }
-
-
-// function validateAddress(address) {
-
-//     $.when(
-//         $.ajax({
-//             async: false,
-//             url: _config.configs.geocodeServiceEndPoint + address,
-//             type: 'get',
-//             dataType: 'json',                        
-//         })
-//     ).done(function(data){        
-//         return true;
-//     }).fail(function(){
-//         return false;
-//     });
-// }
-
-// var dataOperationOptions = {
-//     method : {},
-//     url : "",
-//     type : "get",
-//     dataType : 'json',
-//     data : {},
-//     success : {}, 
-//     fail : {},
-//     inProgress ={}    
-// },
-
-// function dataOperation(method, dataOperationOptions) {
-//     dataOperationOptions.inProgress();
-
-//     $.when(
-//         method()
-//     ).done(function(data){
-//         dataOperationOptions.success(data);
-//     })    
-//     .fail(function(){})
-//     .progress(function(){
-//         dataOperationOptions.inProgress();
-//     })
-// }
 
 exports.testData = testData;
 exports.getEvents = getEvents;
-//exports.validateAddress = validateAddress;
-//exports.dataOperationOptions = dataOperationOptions;
-//exports.dataOperation = dataOperation;
--- js/entries.js
+++ js/entries.js
@@ -3,41 +3,56 @@ var Searchobject = class Searchobject {
     constructor() {
         this.address = "";
         this.lat = "";
-        this.lng ="";
-        this.radius=  "";
-        this.dateStart =  "";
-        this.dateEnd =  "";
-        this.category =  "";
+        this.lng = "";
+        this.radius = "";
+        this.dateStart = "";
+        this.dateEnd = "";
+        this.category = "";
     }
-  };
+};
 
 
-  var Event = class event {
+var Event = class event {
     constructor() {
         this.title = "";
         this.url = "";
-        this.start_time ="";
-        this.stop_time=  "";
-        this.venue_name =  "";
-        this.venue_url =  "";
-        this.description  =  "";
+        this.start_time = "";
+        this.stop_time = "";
+        this.venue_name = "";
+        this.venue_url = "";
+        this.description = "";
 
-        this.venue_address ="";
-        this.latitude=  "";
-        this.longitude =  "";
+        this.venue_address = "";
+        this.latitude = "";
+        this.longitude = "";
 
-        this.image=""        
+        this.image = ""
     }
-  };
+};
 
-  var Thumb = class thumb{
+var Thumb = class thumb {
     constructor() {
         this.width = "";
         this.url = "";
-        this.height ="";
+        this.height = "";
     }
-  }
+};
 
-  exports.Searchobject = Searchobject;
-  exports.Event = Event;
-  exports.Thumb = Thumb;
+var EventfulRoot = class EventfulRootObject {
+    constructor() {
+        this.last_item = "";
+        this.total_items = ""
+        this.first_item = "";
+        this.page_number = "";
+        this.page_size = "";
+        this.page_items = "";
+        this.search_time = "";
+        this.page_count = "";
+        this.events = "";
+    }
+}
+
+exports.Searchobject = Searchobject;
+exports.EventfulRoot = EventfulRoot;
+exports.Event = Event;
+exports.Thumb = Thumb;
